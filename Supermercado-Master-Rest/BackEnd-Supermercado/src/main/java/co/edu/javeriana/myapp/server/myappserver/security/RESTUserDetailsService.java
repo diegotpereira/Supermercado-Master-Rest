c41d648ec668a264866bdc6e55b93581b97ca4a0
T.I <T.I@TI.rsatacadao.local>
2020-08-19 08:51:30 -0300

    alteração no arquivo RESTUserDetailsService.java (limpeza)

--- Supermercado-Master-Rest/BackEnd-Supermercado/src/main/java/co/edu/javeriana/myapp/server/myappserver/security/RESTUserDetailsService.java
+++ Supermercado-Master-Rest/BackEnd-Supermercado/src/main/java/co/edu/javeriana/myapp/server/myappserver/security/RESTUserDetailsService.java
@@ -23,20 +23,11 @@ public class RESTUserDetailsService implements UserDetailsService {
 
 	public RESTUserDetailsService() {
 		super();
-		//users.put("user", new User("user", "password", "ROLE_USER"));
-		//users.put("admin", new User("admin", "password", "ROLE_ADMIN"));
 
 	}
 	@Override
 	public User loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO En este método debería recuperarlse la info del usuario desde la base de datos
-		
-		/*for (Usuario u : this.repository.findAll()) {
-			if(u.getNombre().compareTo(username) == 0) {
-			 	return new User(u.getNombre(),u.getContrasena(),u.getRol());
-			}
-			System.out.println("-----------+++++++++++++++++++++++++++*************************************2");
-		}*/
+	
 		try {
 			Usuario u = this.repository.findByName(username);
 			
@@ -49,9 +40,6 @@ public class RESTUserDetailsService implements UserDetailsService {
 		}catch(Exception e){
 			throw new UsernameNotFoundException("Error");
 		}
-		
-		//return new User("michael","1234","ROLE_CAJERO");
-		
 	}	
 
 }
